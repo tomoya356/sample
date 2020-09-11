7e2bcf3a15cc8ae6da4bfcab257724451f10b757
高森智也 <65030462+tomoya356@users.noreply.github.com>
2020-09-11 16:21:34 +0900

    Initial commit
    練習です

---
+++ ポートフォリオ１/js/main.js
@@ -0,0 +1,14 @@
+// 'use strict';
+// {  
+//   // ローディング画面のdivを取得
+//   var showloading = document.getElementById('showloading');
+// // 画面本体のdivを取得
+// var contents = document.getElementById('contents');
+// // 読み込みが完了したら発動
+// window.addEventListener('load', function () {
+// // loadingのdivを非表示に
+// showloading.style.display = 'none';
+// // contentsのdivを表示
+// contents.classList.remove('hidden');
+// });
+// }
